fb970ceb4d09129475fb5047b8cf11f74acd6bf4
Swethavarnaa U <swethavarnaa@gmail.com>
2023-04-01 17:32:34 +0530

    AWS automation scripts.

--- ebs_autoscale.sh
+++ ebs_autoscale.sh
@@ -3,23 +3,19 @@
 # Set the storage threshold
 THRESHOLD=10
 
-# Get the instance ID
-#INSTANCE_ID=$(curl -s http://ec2IP/latest/meta-data/instance-id)
-
-# Replace the instance ID with your own
+# Set the instance ID
 INSTANCE_ID="i-0123456789abcdef"
 
-
-# Get the all available volumes
+# Get the block device mappings
 BLOCK_DEVICE_MAPPINGS=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[*].Instances[*].BlockDeviceMappings[*].Ebs.VolumeId' --output text)
 
-# Loop through volumes
+# Loop through the block device mappings
 for VOLUME_ID in $BLOCK_DEVICE_MAPPINGS; do
     # Get the current size of the volume
     CURRENT_SIZE=$(aws ec2 describe-volumes --volume-ids $VOLUME_ID --query 'Volumes[*].Size' --output text)
 
-    # Get the current usage of the volume
-    CURRENT_USAGE=$(df -h | grep $VOLUME_ID | awk '{print $5}' | sed 's/%//g')
+    # Get the current usage of the volume (replace this command with one that works on your server)
+    CURRENT_USAGE=$(ssh -i /path/to/key.pem ec2-user@$INSTANCE_IP "df -h | grep $VOLUME_ID | awk '{print $5}' | sed 's/%//g'")
 
     # Check if the current usage is greater than or equal to the threshold
     if (( $CURRENT_USAGE >= $THRESHOLD )); then
